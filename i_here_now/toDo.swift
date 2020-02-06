1dcc732479177e185d94229c0c9348d10b5813a4
umer hamid <smomerhamid@gmail.com>
2020-02-06 02:10:15 -0800

    Initial Commit

---
+++ i_here_now/toDo.swift
@@ -0,0 +1,12 @@
+//
+//  toDo.swift
+//  i_here_now
+//
+//  Created by Umer Hamid on 06/02/2020.
+//  Copyright © 2020 Livheim AB. All rights reserved.
+//
+
+import Foundation
+
+//1.english version is completed
+
